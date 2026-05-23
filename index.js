507b92654fa9d42b175db646ab4242f62614258e
Kakashi7860 <sohelshakh91@gmail.com>
2026-05-23 10:10:57 +0530

    first commit

--- index.js
+++ index.js
@@ -1,62 +1,110 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(express.json())
 
-mongoose.connect("mongodb://localhost:27017/schoolDB")
-console.log("mongodb connected")
+// Middleware
+app.use(express.json());
 
+
+// MongoDB Connection
+mongoose.connect("mongodb://localhost:27017/schoolDB");
+
+console.log("MongoDB Connected");
+
+
+// Schema
 const studentSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    course: String
-})
+  name: String,
+  age: Number,
+  course: String,
+});
 
+
+// Model
 const Student = mongoose.model(
-    "Student",
-    studentSchema
-)
+  "Student",
+  studentSchema
+);
 
-app.get("/",(req,res)=>{
-    res.send("server is running")
-})
 
-app.post("/add-student",(req,res)=>{
-    const newStudent = new Student({
-        name:req.body.name,
-        age:req.body.age,
-        course:req.body.course,
-    })
-    newStudent.save()
-    res.send("student saved")
-})
 
-app.get("/students",(req,res)=>{
-    Student.find().then((data)=>{
-        res.json(data)
-    })
-})
+// HOME ROUTE
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
 
-app.put("/update-student",(req,res)=>{
-    Student.updateOne(
-        {name: req.body.oldName},
-        {name:req.body.newName}
-    ).then(()=>{
-        res.send("student updated")
-    })
-})
 
-app.delete("/delete-student",(req,res)=>{
-    Student.deleteOne({
-        name: req.body.name
-    })
-    .then(()=>{
-        res.send("student deleted")
-    })
-})
 
-app.listen(8000,()=>{
-    console.log("server running on port 8000")
-})
+// CREATE STUDENT
+app.post("/add-student", (req, res) => {
+
+  const newStudent = new Student({
+    name: req.body.name,
+    age: req.body.age,
+    course: req.body.course,
+  });
+
+  newStudent.save();
+
+  res.send("Student Saved Successfully");
+});
+
+
+
+// READ STUDENTS
+app.get("/students", (req, res) => {
+
+  Student.find().then((data) => {
+
+    res.json(data);
+
+  });
+
+});
+
+
+
+// UPDATE STUDENT
+app.put("/update-student", (req, res) => {
+
+  Student.updateOne(
+
+    { name: req.body.oldName },
+
+    {
+      name: req.body.newName,
+    }
+
+  ).then(() => {
+
+    res.send("Student Updated");
+
+  });
+
+});
+
+
+
+// DELETE STUDENT
+app.delete("/delete-student", (req, res) => {
+
+  Student.deleteOne({
+
+    name: req.body.name
+
+  }).then(() => {
+
+    res.send("Student Deleted");
+
+  });
+
+});
+
+
+
+// START SERVER
+app.listen(3000, () => {
+  console.log("Server Running On Port 3000");
+});
