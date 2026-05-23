2aba6d93a5d0a77b8511daa5c6ca03e9deb2dbc0
Kakashi7860 <sohelshakh91@gmail.com>
2026-05-23 09:42:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express();
+
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/schoolDB")
+console.log("mongodb connected")
+
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    course: String
+})
+
+const Student = mongoose.model(
+    "Student",
+    studentSchema
+)
+
+app.get("/",(req,res)=>{
+    res.send("server is running")
+})
+
+app.post("/add-student",(req,res)=>{
+    const newStudent = new Student({
+        name:req.body.name,
+        age:req.body.age,
+        course:req.body.course,
+    })
+    newStudent.save()
+    res.send("student saved")
+})
+
+app.get("/students",(req,res)=>{
+    Student.find().then((data)=>{
+        res.json(data)
+    })
+})
+
+app.listen(8000,()=>{
+    console.log("server running on port 8000")
+})
